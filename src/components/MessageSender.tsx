6ca744d084a13f9c192b5cb6d8930a4e27b3da84
Taran <taranjeet3412@gmail.com>
2024-07-01 02:49:35 +0530

    some-changes

--- src/components/MessageSender.tsx
+++ src/components/MessageSender.tsx
@@ -4,7 +4,7 @@ import { chatProp } from "../pages/Chat"
 
 export default function MessageSender({chat}: {chat: chatProp}) {
     return (
-        <div className="flex gap-2 mx-2 my-5">
+        <div className="flex gap-2 mx-1 my-5">
             <Avatar img={chat.sender.image} />
             <div className="rounded-tl-none rounded-r-xl rounded-b-xl rounded-l-xl bg-white max-w-64 sm:max-w-96 px-4 py-2 shadow-md ">
                 <h5 className='text-sm  text-gray-500'>{chat.message}</h5>
